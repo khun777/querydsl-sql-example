9f0c24df9ec9ba04eb45ddf6ca8a513dc2a668ef
Timo Westkämper <timo.westkamper@mysema.com>
2013-12-03 10:30:08 +0200

    Remove commented out code

--- src/test/java/com/querydsl/example/sql/repository/TweetRepositoryTest.java
+++ src/test/java/com/querydsl/example/sql/repository/TweetRepositoryTest.java
@@ -69,24 +69,4 @@ public class TweetRepositoryTest extends AbstractPersistenceTest {
         assertEquals(1, repository.findAll(tweet.content.contains("#YOLO")).size());
     }
 
-    @Test
-    public void find_list_by_complex_predicate() {
-//        List<String> usernames = Lists.newArrayList("dr_frank", "mike", "maggie", "liza");
-//        List<User> users = Lists.newArrayList();
-//        for (String username : usernames) {
-//            users.add(userRepository.save(new User(username)));
-//        }
-//        User poster = new User("duplo");
-//        userRepository.save(poster);
-//        for (int i = 0; i < 100; ++i) {
-//            repository.save(new Tweet(poster, "spamming @dr_frank " + i, users.subList(0, 1), null));
-//        }
-//        assertTrue(repository.findAll(tweet.mentions.contains(users.get(1))).isEmpty());
-//
-//        assertEquals(100, repository.findAll(tweet.mentions.contains(users.get(0))).size());
-//
-//        assertTrue(repository.findAll(tweet.mentions.any().username.eq("duplo")).isEmpty());
-//
-//        assertEquals(100, repository.findAll(tweet.mentions.any().username.eq("dr_frank")).size());
-    }
 }
